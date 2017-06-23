d3e3ef4098f8f9c992dd0ff9c61d0a2c1c6315b8
Steven van der Merwe <steven.vandermerwe@kaleidocode.co.za>
2017-06-23 14:27:45 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+angular.module('myApp', []);
+
+angular.module('myApp')
+    .controller('myAppCtrl', function($scope) {
+        $scope.title = "Helllloooo";
+    });
---
+++ specs/myappCtrl.spec.js
@@ -0,0 +1,8 @@
+describe('myApp', function() {
+
+    describe('myAppCtrl', function() {
+        it('should return true', function() {
+            expect(true).toBe(true);
+        });
+    });
+});
